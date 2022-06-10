3030ba8bcbe281115c40fce081daf9cf3faf52d8
Sourav <sourav@192.168.1.9>
2022-06-10 06:38:21 +0530

    user logging 2

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,9 +1,9 @@
 import { UPDATE_POSTS } from "./actionTypes";
+import { ApiUrls } from "../helpers/urls";
 
 export function fetchPosts() {
   return (dispatch) => {
-    const url =
-      "https://codeial.codingninjas.com:8000/api/v2/posts?page=1&limit=5";
+    const url = ApiUrls.fetchPosts();
     fetch(url)
       .then((response) => {
         return response.json();
