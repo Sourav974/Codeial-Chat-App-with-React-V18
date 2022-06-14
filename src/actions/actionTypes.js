194b82e3cd2642ec0e0f2538def1951ea96ebd58
Sourav <sourav@Souravs-MacBook-Pro.local>
2022-06-14 14:50:16 +0530

    Edit User: adding actions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,11 +5,14 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILED = "LOGIN_FAILED";
 
-export const AUTHENTICATE_USER = 'AUTHENTICATE_USER'
-export const LOG_OUT = 'LOG_OUT';
+export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
+export const LOG_OUT = "LOG_OUT";
 
 // signup
-export const SIGNUP_START = 'SIGNUP_START'
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
-export const SIGNUP_FAILED = 'SIGNUP_FAILED'
-export const CLEAR_AUTH_STATE = 'CLEAR_AUTH_STATE'
+export const SIGNUP_START = "SIGNUP_START";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAILED = "SIGNUP_FAILED";
+export const CLEAR_AUTH_STATE = "CLEAR_AUTH_STATE";
+
+export const EDIT_USER_SUCESSFUL = "EDIT_USER_SUCESSFUL";
+export const EDIT_USER_FAILED = "EDIT_USER_FAILED";
--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import { fetchPosts } from "../actions/posts";
 import { Home, Navbar, Page404, Login, Signup, Settings } from "./";
 import jwt from "jwt-decode";
 import { authenticateUser } from "../actions/auth";
+import { getAuthTokenFromLocalStorage } from "../helpers/utils";
 
 // const Settings = () => <div>Setting</div>;
 
@@ -43,7 +44,7 @@ class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
 
-    const token = localStorage.getItem("token");
+    const token = getAuthTokenFromLocalStorage();
 
     if (token) {
       const user = jwt(token);
