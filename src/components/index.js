57fb3aeb8fdbeaa730fa442c38016b41b880218f
Sourav <sourav@Souravs-MacBook-Pro.local>
2022-06-17 05:44:26 +0530

    Chat Ui

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,7 @@ import FriendsListItem from "./FriendsListItem";
 import CreatePost from "./CreatePost";
 import Post from "./Post";
 import Comment from "./Comment";
+import Chat from "./Chat";
 
 // import App from "./App";
 
@@ -28,4 +29,5 @@ export {
   CreatePost,
   Post,
   Comment,
+  Chat,
 };
